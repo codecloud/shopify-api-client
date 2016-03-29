c0d7a71098ed709360764d8fab6c45430da6b40d
David Smith <dasmith@rmn.com>
2016-03-29 21:41:56 +0100

    Added setup project files

---
+++ src/Client.php
@@ -0,0 +1,11 @@
+<?php
+namespace Codecloud\ShopifyApiClient;
+
+class Client
+{
+    public function __construct()
+    {
+        echo 'dave' . __NAMESPACE__ . PHP_EOL;
+        exit;
+    }
+}
---
+++ tests/ClientTest.php
@@ -0,0 +1,13 @@
+<?php
+use Codecloud\ShopifyApiClient\Client;
+
+class ClientTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function construct()
+    {
+        
+    }
+}
